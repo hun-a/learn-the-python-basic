30c9c7e2740a9f9fd2375fbbd82b8f54aa4d454c
hun-a <seunghun.chan@gmail.com>
2019-12-23 18:59:33 +0900

    Handle the number type

---
+++ src/01.number.py
@@ -0,0 +1,35 @@
+# https://docs.python.org/3/tutorial/introduction.html#numbers
+
+print(2 + 2)
+print(50 - 5*6)
+print((50 - 5*6) / 4)
+print (8 / 5) # division always returns a floating point number
+
+print(17 / 3) # simple division
+print(17 // 3)  # floor division
+print (17 % 3)  # remainder of the division 
+print (5 * 3 + 2) # result * dividor * remainder
+
+width = 20
+height = 5 * 9
+print(width * height)
+
+'''
+n
+Traceback (most recent call last):
+  File "src/01.number.py", line 23, in <module>
+    n
+NameError: name 'n' is not defined
+'''
+
+print(4 * 3.75 - 1) # full support for floating point
+
+'''
+Below should type in interactive mode
+_ is read only variable.
+tax = 12.5 / 100
+price = 100.50
+price * tax
+price + _
+round(_, 2)
+'''
