411c05cd3e4a94a1af523ae813692837117676ff
hun-a <seunghun.chan@gmail.com>
2019-12-23 19:01:58 +0900

    Handle the String type

---
+++ src/02.string.py
@@ -0,0 +1,9 @@
+# https://docs.python.org/3/tutorial/introduction.html#strings
+
+print('spam eggs')
+print('doesn\'t')
+print("doesn't")
+print('"Yes," they said.')
+print("\"yes,\" they said.")
+print('Isn\'t they said.')
+
