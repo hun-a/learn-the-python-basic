e2a1694404f170da6fc411d236490b563d6b9482
hun-a <seunghun.chan@gmail.com>
2020-01-09 00:19:05 +0900

    Handle the user input/output

---
+++ src/14.user_io.py
@@ -0,0 +1,14 @@
+# https://wikidocs.net/25
+
+a = input()
+print(a)
+
+number = input('Input your number: ')
+print(number)
+
+print("Life" "is" "too short")
+print("Life" + "is" + "too short")
+print("Life", "is", "too short")
+
+for i in range(1, 11):
+  print(i, end=' ')
