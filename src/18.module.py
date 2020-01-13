946dd5124b4cfa5983151cd4117ee1d8c06aadc0
hun-a <seunghun.chan@gmail.com>
2020-01-13 22:31:27 +0900

    Handle the module

---
+++ src/18.module.py
@@ -0,0 +1,18 @@
+# https://wikidocs.net/29
+
+# import mod
+
+# print(mod.add(10, 20))
+
+# print(mod.sub(20, 10))
+
+from mod import *
+import sys
+
+print(add(10, 20))
+print(sub(20, 10))
+print(PI)
+a = Math()
+print(a.solv(2))
+
+print(sys.path)
---
+++ src/mod.py
@@ -0,0 +1,15 @@
+def add(a, b):
+  return a + b
+
+def sub(a, b):
+  return a - b
+
+PI = 3.141592
+
+class Math:
+  def solv(self, r):
+    return PI * (r ** 2)
+
+if __name__ == "__main__":
+  print(add(10, 20))
+  print(sub(20, 10))
